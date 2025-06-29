de93482773c904e9c2cb38845ef4f4ac3c5514f0
unknown <mateuszskora.priv@gmail.com>
2025-06-29 13:26:21 +0200

    Added Cognito Managed UI

--- src/main.tsx
+++ src/main.tsx
@@ -3,15 +3,25 @@ import ReactDOM from "react-dom/client";
 import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
 import "./index.css";
+import outputs from "../amplify_outputs.json";
 import { Amplify } from "aws-amplify";
 import { fetchAuthSession, signInWithRedirect } from "aws-amplify/auth";
 
 Amplify.configure(outputs);
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <Authenticator>
+async function init() {
+  try {
+    await fetchAuthSession();
+  } catch {
+    await signInWithRedirect();
+    return;
+  }
+
+  ReactDOM.createRoot(document.getElementById("root")!).render(
+    <React.StrictMode>
       <App />
-    </Authenticator>
-  </React.StrictMode>
-);
+    </React.StrictMode>
+  );
+}
+
+init();
