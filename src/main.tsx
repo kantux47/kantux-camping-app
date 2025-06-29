a7640d70c4c43c919d8decc8a4b4d4e30f53cafb
unknown <mateuszskora.priv@gmail.com>
2025-06-29 13:25:23 +0200

    Added Cognito Managed UI

--- src/main.tsx
+++ src/main.tsx
@@ -4,7 +4,6 @@ import { Authenticator } from '@aws-amplify/ui-react';
 import App from "./App.tsx";
 import "./index.css";
 import { Amplify } from "aws-amplify";
-import outputs from "../amplify_outputs.json";
 import { fetchAuthSession, signInWithRedirect } from "aws-amplify/auth";
 
 Amplify.configure(outputs);
